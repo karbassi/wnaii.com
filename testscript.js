e10a129457ac8ac925ad8f5909c57db33b177d3a
Patrick Sier <pjsier@gmail.com>
2015-11-21 23:33:44 -0600

    beginning to add leaflet in tests

---
+++ testscript.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([41.88, -87.63], 12);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
