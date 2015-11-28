21f6c81a7bc188c230213691397749535d5a9a16
pjsier <pjsier@gmail.com>
2015-11-28 00:54:44 -0500

    working demo

--- script.js
+++ script.js
@@ -1,264 +1,141 @@
-var TIMEOUT_TIME = 1000;
+L.Icon.Default.imagePath = 'images';
 
-var map;
-var geocoder;
-var timeoutPID;
-var inputElement;
-var canvasElement;
-var answerElement;
-var datalistElement;
-var marker;
-var RESET_TITLE;
-var RESET_CENTER;
-var RESET_ZOOM;
-var FOUND_ZOOM;
-var RESET_NOT_FOUND = "No <b>neighborhood</b> found.";
+var map = L.map('map').setView([41.88, -87.63], 10);
 
-var initialize = function() {
-    RESET_TITLE = document.title;
-    RESET_CENTER = new google.maps.LatLng(41.875696, -87.624207);
-    RESET_ZOOM = 12;
-    FOUND_ZOOM = 15;
-    canvasElement = document.getElementsByTagName('div')[0];
-    datalistElement = document.getElementsByTagName('datalist')[0];
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-    navigator.geolocation.getCurrentPosition(
-        function(position) {
-            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            searchLocation(initialLocation);
-            ga('send', 'event', 'result', 'auto');
-        },
-        function() {
-            inputElement.className = 'invalid';
-            resetMap(RESET_NOT_FOUND);
+var mapzen_key = "search-F2Xk0nk";
+var auto_url = 'https://search.mapzen.com/v1/autocomplete';
+var search_url = 'https://search.mapzen.com/v1/search';
+var inputElement = document.getElementsByTagName('input')[0];
+var dataListEl = document.getElementsByTagName('datalist')[0];
+var formEl = document.getElementsByTagName('form')[0];
+var API_RATE_LIMIT = 1000;
+
+// Load geojson into variable to be used later,
+// async being false will be deprecated later, but works for now
+var chi_json = (function () {
+    var chi_json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "chi_neighborhoods.geojson",
+        'dataType': "json",
+        'success': function (data) {
+            chi_json = data;
         }
-    );
+    });
+    return chi_json;
+})();
 
-    var mapOptions = {
-        center: RESET_CENTER,
-        zoom: RESET_ZOOM,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true
-    };
-
-    map = new google.maps.Map(
-        canvasElement,
-        mapOptions
-    );
-
-    google.maps.Map.prototype.clearOverlays = function() {
-        for (var i = 0; i < markersArray.length; i++ ) {
-            markersArray[i].setMap(null);
-        }
-    };
-
-    geocoder = new google.maps.Geocoder();
-
-    var keyup = function() {
-        updateMap(this.value, false);
-    };
-
-    var blur = function(event) {
-        updateMap(this.value, true);
-        ga('send', 'event', 'form', event.type);
-
-        event.preventDefault();
-        return false;
-    };
-
-    var updateMap = function(address, toFormat) {
-        window.clearTimeout(timeoutPID);
-
-        resetMap('Searching...');
-        timeoutPID = window.setTimeout(function() {
-            searchLocation(address, toFormat);
-        }, TIMEOUT_TIME);
-    };
-
-    var form = document.getElementsByTagName('form')[0];
-    inputElement = document.getElementsByTagName('input')[0];
-    answerElement = document.getElementsByTagName('p')[0];
-
-    addListener(inputElement, 'keyup', keyup);
-    addListener(inputElement, 'blur', blur);
-    addListener(form, 'submit', blur);
+// use HTML5 geolocation on page load, otherwise throw error
+window.onload = function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = [position.coords.latitude, position.coords.longitude];
+      searchNeighborhoods(pos, chi_json);
+      var marker = L.marker([pos[0], pos[1]]).addTo(map);
+      map.setView([pos[0], pos[1]], 15);
+    });
+  }
+  else {
+    console.log("Geolocation is not supported");
+  }
 };
 
-var centerMap = function() {
-    var position = RESET_CENTER;
-    var zoom = RESET_ZOOM;
+// Use existing throttle function to load autocomplete Mapzen results on keyup
+inputElement.addEventListener('keyup', throttle(searchAddress, API_RATE_LIMIT));
 
-    if (marker != null) {
-        position = marker.position;
-        zoom = FOUND_ZOOM;
-    }
+// Use Mapzen search API on user hitting enter, give "true" argument to call to different endpoint
+formEl.addEventListener('submit', function() { searchAddress(true); });
 
-    map.setCenter(position);
-    map.setZoom(zoom);
+function searchAddress(submitAddr) {
+  var params = {
+    api_key: mapzen_key,
+    "focus.point.lon": -87.63,
+    "focus.point.lat": 41.88,
+    text: inputElement.value
+  };
+  // if optional argument supplied, call search endpoint
+  if (submitAddr === true) {
+    callPelias(search_url, params);
+  }
+  else if (inputElement.value.length > 0) {
+    callPelias(auto_url, params);
+  }
 };
 
-var searchLocation = function(address, toFormat) {
-    if (address.length === 0) {
-        resetMap();
-        return;
+function callPelias(url, search_params) {
+  AJAX.request(url, search_params, function (err, results) {
+    if (err) {
+      console.log(err);
     }
-
-    var findType = function(result, type) {
-        var i = 0;
-        var l = result.address_components.length;
-        var component;
-
-        for(; i < l; i++) {
-            component = result.address_components[i];
-            if (component.types[0] == type) {
-                return component;
-            }
+    // if autocomplete url provided
+    if (url === auto_url) {
+      if (results && results.features) {
+        // remove all children of datalist until empty, fast way of emptying
+        while (dataListEl.firstChild) {
+          dataListEl.removeChild(dataListEl.firstChild);
+        };
+        var optionLength = 5;
+        if (results.features.length < 5) {
+          optionLength = results.features.length;
         }
-
-        return false;
-    };
-
-    var findSuggestions = function(results) {
-        // var suggestions = [];
-        var i = 0;
-        // var l = results.length;
-        var l = 3;
-
-        // Clear list
-        datalistElement.innerHTML = '';
-
-        for(; i < l; i++) {
-            if (results[i] == undefined) {
-                continue;
-            }
-
-            // suggestions.push(results[i].formatted_address);
-            var option = document.createElement('option');
-            option.value = results[i].formatted_address;
-            option.text = results[i].formatted_address;
-            datalistElement.appendChild(option);
-            // str += '<option value="' + results[i].formatted_address + '">' + results[i].formatted_address + '</option>';
-        }
-
-    };
-
-    var callback = function(results, status) {
-        if (status != google.maps.GeocoderStatus.OK) {
-            resetMap();
-            ga('send', 'event', 'result', 'failed');
-            return;
-        }
-
-        findSuggestions(results);
-
-        var result = results[0];
-
-        console.log(results);
-
-        var neighborhood = {};
-
-        for (var i = 0; i < results.length; i++) {
-            var hood = findType(results[i], 'neighborhood').long_name;
-            if (hood && hood.length > 0) {
-                neighborhood[hood] = true;
-            }
-        }
-
-        neighborhood = Object.keys(neighborhood);
-
-        if (!neighborhood || !neighborhood.length) {
-            inputElement.className = 'invalid';
-            resetMap(RESET_NOT_FOUND);
-            return;
-        }
-
-        inputElement.className = 'valid';
-
-        var country = findType(result, 'country').long_name;
-        var administrative_area_level_1 = findType(result, 'administrative_area_level_1').long_name;
-
-        ga('send', 'event', 'country', country);
-        ga('send', 'event', 'state', administrative_area_level_1);
-        ga('send', 'event', 'neighborhood', neighborhood);
-
-        map.setZoom(FOUND_ZOOM);
-        map.setCenter(result.geometry.location);
-
-        if (marker == null) {
-            marker = new google.maps.Marker({
-                map: map,
-                position: result.geometry.location
-            });
-        } else {
-            marker.setPosition(result.geometry.location);
-        }
-
-        updateNeighborhood(neighborhood);
-
-        if (toFormat) {
-            inputElement.value = result.formatted_address;
-        }
-    };
-
-    var options = { 'address': address };
-
-    if (typeof address != 'string') {
-        options = { 'latLng': address };
+        // Loop through first five results (or fewer) and add to options
+        // TO-DO: Figure out how to display options that aren't datalist exact matches
+        for (var i = 0; i < optionLength; ++i) {
+          var option = document.createElement('option');
+          option.value = results.features[i].properties.label;
+          option.text = results.features[i].properties.label;
+          dataListEl.appendChild(option);
+        };
+      }
     }
-
-    geocoder.geocode(options, callback);
+    // If search url provided
+    else if (url === search_url) {
+      if (results && results.features) {
+        var pos = [results.features[0].geometry.coordinates[1], results.features[0].geometry.coordinates[0]];
+        searchNeighborhoods(pos, chi_json);
+      }
+    }
+  });
 };
 
-var resetMap = function(message) {
-    document.title = RESET_TITLE;
-
-    if (message) {
-        answerElement.style.display = 'block';
-        answerElement.innerHTML = message;
-    } else {
-        answerElement.style.display = 'none';
-        answerElement.innerHTML = '';
+function searchNeighborhoods(position, neighborhoods) {
+  // create arbitrary geoJSON point to submit to turfjs function
+  var pt = {
+    "type": "Feature",
+    "properties": {
+      "marker-color": "#0f0"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": []
     }
+  };
 
-    if (marker) {
-        marker.setMap(null);
-        marker = null;
+  pt["geometry"]["coordinates"] = [position[1], position[0]];
+
+  // Get var of p tag that will hold neighborhood answer
+  var answerElement = document.getElementsByTagName('p')[0];
+  answerElement.style.display = 'block';
+
+  // Create point marker and add to map
+  var marker = L.marker([position[0], position[1]]).addTo(map);
+  map.setView([position[0], position[1]], 15);
+
+  // Loop through all GeoJSON features, break if one selected, and change answerElement
+  for (var i = 0; i < neighborhoods.features.length; ++i) {
+    if (turf.inside(pt, neighborhoods.features[i])) {
+      answerElement.innerHTML = "You are in " + '<b>' + neighborhoods.features[i].properties.name + '</b>.';
+      break;
     }
-
-    centerMap();
+    else {
+      answerElement.innerHTML = "You are outside Chicago";
+    }
+  };
 };
-
-var updateNeighborhood = function(neighborhood) {
-
-    if (!neighborhood || neighborhood.length === 0) {
-        resetMap();
-        return;
-    }
-
-    var text = 'You are in ';
-
-    document.title = text + neighborhood.join(', ') + '.';
-
-    answerElement.style.display = 'block';
-    answerElement.innerHTML = text + '<b>' + neighborhood.join(', ') + '</b>.';
-
-};
-
-google.maps.event.addDomListener(window, 'load', initialize);
-google.maps.event.addDomListener(window, 'resize', centerMap);
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-40976307-2', 'wnaii.com');ga('send', 'pageview');
-
-/**
- * Utility to wrap the different behaviors between W3C-compliant browsers
- * and IE when adding event handlers.
- *
- * @param {Object} element Object on which to attach the event listener.
- * @param {string} type A string representing the event type to listen for
- *     (e.g. load, click, etc.).
- * @param {function()} callback The function that receives the notification.
- */
-function addListener(element, type, callback) {
-    if (element.addEventListener) element.addEventListener(type, callback);
-    else if (element.attachEvent) element.attachEvent('on' + type, callback);
-}
