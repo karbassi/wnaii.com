5057974c5e5074b12eb1a06eaee0545542a83a08
Patrick Sier <pjsier@gmail.com>
2016-02-07 08:31:53 -0600

    swapped mapzen key, removed contrib, moved throttle function into script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ var inputElement = document.getElementById("addr-search");
 inputElement.disabled = true;
 inputElement.placeholder = "Getting location...";
 
-var mapzen_key = "search-F2Xk0nk";
+var mapzen_key = "search-6xXbnNY";
 var auto_url = 'https://search.mapzen.com/v1/autocomplete';
 var search_url = 'https://search.mapzen.com/v1/search';
 
@@ -195,6 +195,41 @@ function searchNeighborhoods(position, neighborhoods) {
       answerElement.innerHTML = "You are in " + '<b>' + neighborhoods.features[i].properties.name + '</b>.';
       break;
     }
+  };
+};
+
+/*
+* throttle Utility function (borrowed from underscore)
+*/
+function throttle (func, wait, options) {
+  var context, args, result;
+  var timeout = null;
+  var previous = 0;
+  if (!options) options = {};
+  var later = function () {
+    previous = options.leading === false ? 0 : new Date().getTime();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  return function () {
+    var now = new Date().getTime();
+    if (!previous && options.leading === false) previous = now;
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
   };
 };
 
