5c916992dc7748a787f91a278e4ec7527627b14d
Ali Karbassi <ali@karbassi.com>
2020-05-01 21:49:30 -0500

    Update tilemap
    - Update tilemap
    - Update library files to latest version
    - Fix some CSS/JS

--- README.md
+++ README.md
@@ -1,27 +1,9 @@
 # wnaii.com
 
-## What Neighborhood Am I In? (Chicago)
+## What Neighborhood Am I In?
 
-Simple web application to tell users which Chicago neighborhood they're in, search
-for the neighborhoods of other addresses, and see an overall map of Chicago's neighborhoods.
+Simple web application to tell users which USA neighborhood they're in, search for the neighborhoods of other addresses, and see an overall map of neighborhoods.
 
 Application is live at http://wnaii.com/
 
 Neighborhood Boundaries provided by [Zillow Neighborhood Boundaries](https://www.zillow.com/howto/api/neighborhood-boundaries.htm) under [Creative Commons license](https://creativecommons.org/licenses/by-sa/3.0/).
-
-## Setup
-
-In order to download and contribute, clone the repo, and run:
-
-```bash
-cd wnaii.com
-npm install
-```
-
-Then, in order to run it on a local web server, run:
-
-```python
-python -m $(python -c 'import sys; print("http.server" if sys.version_info[:2] > (2,7) else "SimpleHTTPServer")')
-```
-
-And navigate to http://localhost:8000
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,201 +1,194 @@
 const center = {
-	lat: 41.88,
-	lon: -87.63,
+  lat: 41.88,
+  lon: -87.63,
 };
 
-const loadingElement = document.getElementById('loading');
-const resultsElement = document.getElementById('found');
-const locationElement = document.getElementById('location');
+const loadingElement = document.querySelector(".loading");
+const resultsElement = document.querySelector(".found");
+const locationElement = document.querySelector(".location");
 const states_hash = {
-	'Alabama': 'AL',
-	'Alaska': 'AK',
-	'Arizona': 'AZ',
-	'Arkansas': 'AR',
-	'California': 'CA',
-	'Colorado': 'CO',
-	'Connecticut': 'CT',
-	'Delaware': 'DE',
-	'District Of Columbia': 'DC',
-	'Florida': 'FL',
-	'Georgia': 'GA',
-	'Hawaii': 'HI',
-	'Idaho': 'ID',
-	'Illinois': 'IL',
-	'Indiana': 'IN',
-	'Iowa': 'IA',
-	'Kansas': 'KS',
-	'Kentucky': 'KY',
-	'Louisiana': 'LA',
-	'Maine': 'ME',
-	'Maryland': 'MD',
-	'Massachusetts': 'MA',
-	'Michigan': 'MI',
-	'Minnesota': 'MN',
-	'Mississippi': 'MS',
-	'Missouri': 'MO',
-	'Montana': 'MT',
-	'Nebraska': 'NE',
-	'Nevada': 'NV',
-	'New Hampshire': 'NH',
-	'New Jersey': 'NJ',
-	'New Mexico': 'NM',
-	'New York': 'NY',
-	'North Carolina': 'NC',
-	'North Dakota': 'ND',
-	'Ohio': 'OH',
-	'Oklahoma': 'OK',
-	'Oregon': 'OR',
-	'Pennsylvania': 'PA',
-	'Rhode Island': 'RI',
-	'South Carolina': 'SC',
-	'South Dakota': 'SD',
-	'Tennessee': 'TN',
-	'Texas': 'TX',
-	'Utah': 'UT',
-	'Vermont': 'VT',
-	'Virginia': 'VA',
-	'Washington': 'WA',
-	'West Virginia': 'WV',
-	'Wisconsin': 'WI',
-	'Wyoming': 'WY'
-}
-
+  Alabama: "AL",
+  Alaska: "AK",
+  Arizona: "AZ",
+  Arkansas: "AR",
+  California: "CA",
+  Colorado: "CO",
+  Connecticut: "CT",
+  Delaware: "DE",
+  "District Of Columbia": "DC",
+  Florida: "FL",
+  Georgia: "GA",
+  Hawaii: "HI",
+  Idaho: "ID",
+  Illinois: "IL",
+  Indiana: "IN",
+  Iowa: "IA",
+  Kansas: "KS",
+  Kentucky: "KY",
+  Louisiana: "LA",
+  Maine: "ME",
+  Maryland: "MD",
+  Massachusetts: "MA",
+  Michigan: "MI",
+  Minnesota: "MN",
+  Mississippi: "MS",
+  Missouri: "MO",
+  Montana: "MT",
+  Nebraska: "NE",
+  Nevada: "NV",
+  "New Hampshire": "NH",
+  "New Jersey": "NJ",
+  "New Mexico": "NM",
+  "New York": "NY",
+  "North Carolina": "NC",
+  "North Dakota": "ND",
+  Ohio: "OH",
+  Oklahoma: "OK",
+  Oregon: "OR",
+  Pennsylvania: "PA",
+  "Rhode Island": "RI",
+  "South Carolina": "SC",
+  "South Dakota": "SD",
+  Tennessee: "TN",
+  Texas: "TX",
+  Utah: "UT",
+  Vermont: "VT",
+  Virginia: "VA",
+  Washington: "WA",
+  "West Virginia": "WV",
+  Wisconsin: "WI",
+  Wyoming: "WY",
+};
 
 let map;
 let point;
 let markerLayer;
 
 // Use HTML5 geolocation on page load, otherwise throw error
-window.addEventListener('load', init);
+window.addEventListener("load", init);
 
 function init() {
-	createMap(center.lat, center.lon);
-	getGeolocation();
+  createMap(center.lat, center.lon);
+  getGeolocation();
 }
 
 function createMap(lat, lon) {
-	map = L.map('map', {
-		'center': [lat, lon],
-		'zoom': 15,
-		'attributionControl': false,
-	});
+  map = L.map("map", {
+    center: [lat, lon],
+    zoom: 15,
+    attributionControl: false,
+  });
 
-	L.tileLayer('//maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png').addTo(map);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-	markerLayer = L.geoJson().addTo(map);
+  markerLayer = L.geoJson().addTo(map);
 }
 
 function getGeolocation() {
-	var options = {
-		enableHighAccuracy: false,
-		timeout: 5000,
-		maximumAge: 0,
-	};
+  const options = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0,
+  };
 
-	function success(position) {
-		const lat = position.coords.latitude;
-		const lon = position.coords.longitude;
+  const success = function (position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
 
-		centerMap(lat, lon);
-		createPoint(lat, lon);
-		reverseGeo(lat, lon);
-	}
+    centerMap(lat, lon);
+    createPoint(lat, lon);
+    reverseGeo(lat, lon);
+  };
 
-	function error(err) {
-		console.error(`ERROR(${err.code}): ${err.message}`);
-	}
+  const error = function (err) {
+    console.error(`ERROR(${err.code}): ${err.message}`);
+  };
 
-	navigator.geolocation.getCurrentPosition(success, error, options);
+  navigator.geolocation.getCurrentPosition(success, error, options);
 }
-
 
 function centerMap(lat, lon) {
-	map.setView([lat, lon], 15);
+  map.setView([lat, lon], 15);
 }
-
 
 function reverseGeo(lat, lon) {
-	const URL = `https://api.bigdatacloud.net/data/reverse-geocode-client
+  const URL = `https://api.bigdatacloud.net/data/reverse-geocode-client
 				?latitude=${lat}&longitude=${lon}&localityLanguage=en`;
 
-	fetch(URL)
-		.then(response => response.json())
-		.then(data => {
-			const state = states_hash[data.principalSubdivision]
-			loadGeoJSON(state);
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const state = states_hash[data.principalSubdivision];
+      loadGeoJSON(state);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
-
 
 function loadGeoJSON(state) {
-	const URL = `./assets/geo/${state.toLowerCase()}.geojson`;
+  const URL = `./assets/geo/${state.toLowerCase()}.geojson`;
 
-	fetch(URL)
-		.then(response => response.json())
-		.then(neighborhoods => {
-			parseGeoData(neighborhoods);
-			searchNeighborhoods(neighborhoods);
-		})
-		.catch(error => {
-			console.log(error);
-		});
+  fetch(URL)
+    .then((response) => response.json())
+    .then((neighborhoods) => {
+      parseGeoData(neighborhoods);
+      searchNeighborhoods(neighborhoods);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-
 function parseGeoData(neighborhoods) {
-	const geo = L.geoJson(neighborhoods, {
-		style: {
-			color: 'rgba(255, 0, 0, 0.4)',
-			weight: 1.5
-		},
-		function (feature, layer) {
-			layer.bindPopup(
-				`<strong>Neighborhood:</strong> ${feature.properties.Name}`
-			);
-		}
-	});
+  const geo = L.geoJson(neighborhoods, {
+    style: {
+      color: "rgba(255, 0, 0, 0.4)",
+      weight: 1.5,
+    },
+    function(feature, layer) {
+      layer.bindPopup(
+        `<strong>Neighborhood:</strong> ${feature.properties.Name}`
+      );
+    },
+  });
 
-	map.addLayer(geo);
+  map.addLayer(geo);
 
-	const overlay = {
-		'Neighborhoods': geo
-	};
+  const overlay = {
+    Neighborhoods: geo,
+  };
 
-	L.control.layers(null, overlay).addTo(map);
+  L.control.layers(null, overlay).addTo(map);
 }
 
 function createPoint(lat, lon) {
-	// Create arbitrary geoJSON point to submit to turfjs function
-	point = {
-		type: 'Feature',
-		properties: {
-			'marker-color': '#00FF00'
-		},
-		geometry: {
-			type: 'Point',
-			coordinates: [lon, lat],
-		},
-	};
+  // Create arbitrary geoJSON point to submit to turfjs function
+  point = {
+    type: "Feature",
+    properties: {
+      "marker-color": "#00FF00",
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [lon, lat],
+    },
+  };
 
-	markerLayer.addData(point);
+  markerLayer.addData(point);
 }
 
-
 function searchNeighborhoods(neighborhoods) {
-	// Loop through all GeoJSON features, break if one selected, and change answerElement
-	for (let i = 0; i < neighborhoods.features.length; i++) {
-		let feature = neighborhoods.features[i];
-		if (gju.pointInPolygon(point.geometry, feature.geometry)) {
-			// Get var of p tag that will hold neighborhood answer
-			loadingElement.style.display = 'none';
-			resultsElement.style.display = 'block';
+  // Loop through all GeoJSON features, break if one selected, and change answerElement
+  for (let i = 0; i < neighborhoods.features.length; i++) {
+    const feature = neighborhoods.features[i];
+    if (gju.pointInPolygon(point.geometry, feature.geometry)) {
+      // Get var of p tag that will hold neighborhood answer
+      loadingElement.style.display = "none";
+      resultsElement.style.display = "block";
 
-			locationElement.innerText = feature.properties.Name;
-			break;
-		}
-	}
+      locationElement.innerText = feature.properties.Name;
+      break;
+    }
+  }
 }
